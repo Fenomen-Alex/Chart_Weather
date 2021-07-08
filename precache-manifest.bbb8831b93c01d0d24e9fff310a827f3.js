d3ccdc8ac8a05676b51a65c530d5d39cfb3efcf0
Fenomen-Alex <alexfenomen@gmail.com>
2021-07-08 23:21:48 +0300

    Updates

---
+++ precache-manifest.bbb8831b93c01d0d24e9fff310a827f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b190a9ca6af43506375c15566667280f",
+    "url": "/Chart_Weather/index.html"
+  },
+  {
+    "revision": "2f7e9066b1043497270e",
+    "url": "/Chart_Weather/static/css/main.d3ddb0d8.chunk.css"
+  },
+  {
+    "revision": "2948059e0f6fd26b50b3",
+    "url": "/Chart_Weather/static/js/2.aeaa38f7.chunk.js"
+  },
+  {
+    "revision": "45320c86b95fd616869bda206b3dd9d5",
+    "url": "/Chart_Weather/static/js/2.aeaa38f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f7e9066b1043497270e",
+    "url": "/Chart_Weather/static/js/main.5c22fc58.chunk.js"
+  },
+  {
+    "revision": "915094013d0a9ce625d6",
+    "url": "/Chart_Weather/static/js/runtime-main.3b0065ee.js"
+  }
+]);
