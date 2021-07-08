ed3112472dd4ff7fbc1e86a1cf5ae4932bcf36b1
Fenomen-Alex <alexfenomen@gmail.com>
2021-07-08 20:53:11 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+import './App.css';
+
+// openweathermap.org
+// https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,minutely&units=imperial&appid=bb96c7f9ac6f57dc00333727c5407547
+// api key: bb96c7f9ac6f57dc00333727c5407547
+
+// google maps api
+// api key: AIzaSyDp4os33WF6-4d-xFVyL0HsUUHN7dOml_w
+
+const App = () => {
+  const [latLng, setLatLng] = useState(null);
+
+  return (
+    <div className="app">
+      {/* form goes here */}
+      <GeoForm setLatLng={setLatLng} />
+      {/* chart goes here */}
+      {latLng && <WeatherChart latLng={latLng}/>}
+    </div>
+  );
+}
+
+export default App;
